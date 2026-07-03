693e7dd9c0e047f9a16199ef9415bfc948bdb53d
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-07-03 22:57:35 +0100

    Refactor API to use Hono framework with GraphQL

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,11 @@
-import { ponder } from "ponder:registry";
+import { db } from "ponder:api";
+import schema from "ponder:schema";
 import { graphql } from "ponder";
+import { Hono } from "hono";
 
-ponder.use("/graphql", graphql());
-ponder.use("/", graphql());
+const app = new Hono();
+
+app.use("/", graphql({ db, schema }));
+app.use("/graphql", graphql({ db, schema }));
+
+export default app;
